a45846792e9203f1e2650fb4f7f7e72ec6c2778c
JosephSIN <joseph.sin@yandex.by>
2019-09-28 14:57:09 +0300

    Remaked Entity bindings Remaked WeekGraphic string days method

--- HakatonSolution/Common/BindingEntities/PassengerPathRefPathUserBinding.cs
+++ HakatonSolution/Common/BindingEntities/PassengerPathRefPathUserBinding.cs
@@ -2,12 +2,15 @@
 
 namespace BLL.Implementations.DataAccess.BindingEntities
 {
-    public class PassengerPathRefUserBinding
+    public class PassengerPathRefPathUserBinding
     {
         public int PassengerPathRefId { get; set; }
         public PassengerPathRef PassengerPathRef { get; set; }
 
+        public int PathId { get; set; }
+        public Path Path { get; set; }
+
         public int UserId { get; set; }
-        public User User { get; set; }
+        public Path User { get; set; }
     }
 }
