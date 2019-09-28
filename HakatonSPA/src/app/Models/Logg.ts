930ae8a08ee7dc58819e01d0e3c4943f99ad449c
ZhenyaTrif <nomand144@gmail.com>
2019-09-28 14:06:15 +0300

    auth

---
+++ HakatonSPA/src/app/Models/Logg.ts
@@ -0,0 +1,4 @@
+export class Logg{
+    email: string;
+    password: string;
+}
--- HakatonSPA/src/app/app.module.ts
+++ HakatonSPA/src/app/app.module.ts
@@ -2,28 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AngularYandexMapsModule } from 'angular8-yandex-maps';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SityRoutesComponent } from './sity-routes/sity-routes.component';
 import { SityRoutesService } from './sity-routes/Services/sity-routes.service';
 import { MapPartComponent } from './map-part/map-part.component';
+import { AccountService } from './services/account.service';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SityRoutesComponent,
-    MapPartComponent
+    MapPartComponent,
+    AccountComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    AppRoutingModule,
-    AngularYandexMapsModule.forRoot('d3786fc7-5b58-4366-9a10-6410d91bc85f')
+    AppRoutingModule
   ],
-  providers: [SityRoutesService],
+  providers: [SityRoutesService, AccountService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
