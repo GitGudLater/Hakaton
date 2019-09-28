5b40ee40f813ce765d5c2d845749770d6a8b4011
ZhenyaTrif <nomand144@gmail.com>
2019-09-28 11:02:46 +0300

    add map

--- HakatonSPA/src/app/app.module.ts
+++ HakatonSPA/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularYandexMapsModule } from 'angular8-yandex-maps';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularYandexMapsModule.forRoot('d3786fc7-5b58-4366-9a10-6410d91bc85f')
   ],
   providers: [],
   bootstrap: [AppComponent]
