20b01c00a30de037dc86a6d5c3dce72d307f2d25
ZhenyaTrif <nomand144@gmail.com>
2019-09-28 11:33:53 +0300

    add base map

--- HakatonSPA/src/app/app.module.ts
+++ HakatonSPA/src/app/app.module.ts
@@ -2,22 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularYandexMapsModule } from 'angular8-yandex-maps';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SityRoutesComponent } from './sity-routes/sity-routes.component';
 import { SityRoutesService } from './sity-routes/Services/sity-routes.service';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SityRoutesComponent
+    SityRoutesComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularYandexMapsModule.forRoot('d3786fc7-5b58-4366-9a10-6410d91bc85f')
   ],
   providers: [SityRoutesService],
   bootstrap: [AppComponent]
